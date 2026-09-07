89f509793c7de3aebf2f20fb571d67ded3373de8
Ignacio Guardines <iguardines@gmail.com>
2026-09-07 18:09:35 -0300

    testing cd on render

--- src/main/java/com/example/urlshortener/web/UrlController.java
+++ src/main/java/com/example/urlshortener/web/UrlController.java
@@ -9,7 +9,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-
+// prueba de auto-deploy
 @RestController
 public class UrlController {
     private final ShortUrlService service;
